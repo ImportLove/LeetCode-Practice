7f153a7b0fdc420d8effa4ff0ae9695cff1f725e
zixiangz <zixiangz@umich.edu>
2021-08-27 10:41:50 +0800

    for test only

--- Q756.cpp
+++ Q756.cpp
@@ -1,8 +1,13 @@
 // 756. Pyramid Transition Matrix
 
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     bool pyramidTransition(string bottom, vector<string>& allowed) {
-
+        // comment
     }
 };
